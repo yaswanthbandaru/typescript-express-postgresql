2b7172383adb736a4a8f6d9d513cddca924969ba
yaswanthbandaru <yaswanthbandaru424@gmail.com>
2024-09-06 15:42:08 +0530

    Added postgraphile to system for api usage

--- src/app.ts
+++ src/app.ts
@@ -1,78 +1,26 @@
-// const database = require('./db/db'); // Import the database connection
+require('dotenv').config();
 const express = require('express');
-import { AppDataSource } from "./data-source";
-import { User } from './entity/User';
+const { postgraphile } = require('postgraphile');
+// import { AppDataSource } from "./data-source";
+// import { User } from './entity/User';
+
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 
-// Connect to PostgreSQL database
-AppDataSource.initialize()
-    .then(async () => {
-        console.log('Data source has been initialiazed')
+app.use(
+    postgraphile(
+        process.env.DB_URL,
+        'public',
+        {
+            graphiql: true,
+            enhanceGraphiql: true,
+        }
+    )
+);
 
-        // Define the CRUD operations
-        app.get("/users", async (req: any, res: any) => {
-            const userRepository = AppDataSource.getRepository(User);
-            const users = await userRepository.find();
-            console.log(users);
-            res.json(users);
-        })
-
-        app.post("/users", async (req: any, res: any) => {
-            const userRepository = AppDataSource.getRepository(User);
-            const newUser = userRepository.create(req.body); 
-            const result = await userRepository.save(newUser);
-            console.log(result);
-            res.json(result);
-        })
-
-        app.put("/users/:id", async (req: any, res: any) => {
-            const userRepository = AppDataSource.getRepository(User);
-            const { id } = req.params;
-            const { firstname, lastname, email } = req.body;
-            const user = await userRepository.findOneBy({ id: parseInt(id) })
-            if(!user) return res.status(404).send("User not found")
-            user.firstname = firstname
-            user.lastname = lastname
-            user.email = email
-            const updateUser = await userRepository.save(user)
-            console.log(updateUser)
-            res.json(updateUser)
-        })
-
-        app.delete("/users/:id", async (req: any, res: any) => {
-            const userRepository = AppDataSource.getRepository(User)
-            const { id } = req.params;
-            const user = await userRepository.findOneBy({ id: parseInt(id) })
-            if(!user) { return res.status(404).send("User not found")}
-            await userRepository.remove(user)
-            res.send("User deleted successfully")
-        })
-
-        app.listen(port, () => {
-            console.log(`Server is running on https://localhost:${port}`);
-        })
-    }).catch((error: any) => {
-        console.error("Error during Data Source initialization ", error);
-    })
-
-
-/*
-
-Post Request:
-    curl -X POST http://localhost:3000/users -H "Content-Type: application/json" -d '{"firstname": "John", "lastname": "Doe", "email": "john@example.com"}'
-
-
-GET Request:
-    curl -X GET http://localhost:3000/users
-
-PUT Request:
-    curl -X PUT http://localhost:3000/users/1 -H "Content-Type: application/json" -d '{"firstname": "Joey", "lastname": "Doe", "email": "joeydoe@example.com"}'
-
-
-DELETE Request:
-    curl -X DELETE http://localhost:3000/users/1 
-*/
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+});
